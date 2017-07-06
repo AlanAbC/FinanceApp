42f9eb57045a9e9d4a0063273312eb9ef798cd49
alana <alanjiraya77@gmail.com>
2017-07-06 04:45:06 -0500

    Se agrego login y registro

--- app/src/main/java/com/claresti/financeapp/Login.java
+++ app/src/main/java/com/claresti/financeapp/Login.java
@@ -1,13 +1,45 @@
 package com.claresti.financeapp;
 
+import android.content.Intent;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-
+    TextView btnRegistro;
+    //Declaracion de variables para el control de bottom sheet
+    private Button btnConBottomSheet;
+    private LinearLayout bottomSheet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
+        btnRegistro=(TextView)findViewById(R.id.btn_registro);
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this, Registro.class);
+                startActivity(i);
+            }
+        });
+
+        //Llamada de las variables para el control de bottomsheet
+        bottomSheet = (LinearLayout)findViewById(R.id.bottomSheet);
+        final BottomSheetBehavior bsb = BottomSheetBehavior.from(bottomSheet);
+        bsb.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        //Control para esconder bottomsheet
+        btnConBottomSheet=(Button)findViewById(R.id.btnConBottomSheet);
+        btnConBottomSheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                bsb.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        });
     }
 }
