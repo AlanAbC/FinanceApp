9001379c7ca1581c1e5325a0dc3270a14c6a1236
alana <alanjiraya77@gmail.com>
2017-07-06 16:56:24 -0500

    Se agrego despliegue de movimientos

---
+++ app/src/main/java/com/claresti/financeapp/DespliegueMovimientos.java
@@ -0,0 +1,13 @@
+package com.claresti.financeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DespliegueMovimientos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_despliegue_movimientos);
+    }
+}
